8b7489561878b035d352dbf92300903188d8a619
joshinils <joshua.laskowski@web.de>
2018-02-17 11:24:50 +0100

    done? with matrix

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,15 +11,20 @@ public class Main extends Application {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args) throws Exception {
+/*
 		Matrix m=new Matrix(3, 2, 0.1);
 		Matrix n=new Matrix(2, 3, -0.1);
 		m.set(0, 0, 1);
@@ -30,11 +35,24 @@ public class Main extends Application {
 		n.set(0, 1, 6);
 		n.set(1, 0, 7);
 		n.set(1, 1, 8);
-		System.out.println(m);
-		System.out.println(n);
-		m=Matrix.mult(m, n);
+		System.out.println("m :\n"+m);
+		System.out.println("n :\n"+n);
+		System.out.println("m*n :\n"+Matrix.mult(m, n));
+		System.out.println("m^T :\n"+Matrix.transpose(m));
+		System.out.println("n^T :\n"+Matrix.transpose(n));
+		System.out.println("m^T*n^T :\n"+Matrix.mult(Matrix.transpose(m), Matrix.transpose(n)));
 		
-		System.out.println(m);
-		//launch(args);
+		System.out.println(Matrix.scale(Matrix.eye(3, 4), 2));
+		
+		System.out.println("clone test");
+		
+		Matrix c=Matrix.clone(m);
+		c=Matrix.scale(c, 5);
+		System.out.println("m:\n"+m);
+		System.out.println("c:\n"+c);
+*/
+		
+		launch(args);
+		System.exit(0); // leave and dont gunk up ram
 	}
 }
--- src/application/Matrix.java
+++ src/application/Matrix.java
@@ -12,9 +12,9 @@ public class Matrix
 	public Matrix(int zeilen, int spalten, double init)
 	{
 		if (zeilen<1)
-		{	throw(new IndexOutOfBoundsException("Matrix::zeilen war zu klein: "+zeilen));	}
+		{	throw(new IndexOutOfBoundsException("Matrix::Matrix zeilen war zu klein: "+zeilen));	}
 		if (spalten<1)
-		{	throw(new IndexOutOfBoundsException("Matrix::spalten war zu klein: "+spalten));	}
+		{	throw(new IndexOutOfBoundsException("Matrix::Matrix spalten war zu klein: "+spalten));	}
 
 		data=new double[zeilen][spalten];
 		
@@ -26,9 +26,9 @@ public class Matrix
 	public static Matrix add(Matrix a, Matrix b) throws Exception
 	{
 		if(a.data.length!=b.data.length)
-			throw(new Exception("Matrix::Dimensionen der Zeilen haut nicht hin! "+a.data.length+"*x != "+b.data.length+"*x"));
+			throw(new Exception("Matrix::add Dimensionen der Zeilen haut nicht hin! "+a.data.length+"*x != "+b.data.length+"*x"));
 		if(a.data[0].length!=b.data[0].length)
-			throw(new Exception("Matrix::Dimensionen der Spalten haut nicht hin! x*"+a.data[0].length+" != x*"+b.data[0].length));
+			throw(new Exception("Matrix::add Dimensionen der Spalten haut nicht hin! x*"+a.data[0].length+" != x*"+b.data[0].length));
 		
 		Matrix erg = new Matrix(a.data.length, a.data[0].length);
 		
@@ -45,20 +45,63 @@ public class Matrix
 	public static Matrix mult(Matrix a, Matrix b) throws Exception
 	{
 		if(a.data.length!=b.data[0].length)
-			throw(new Exception("Matrix::Dimensionen der Zeilen haut nicht hin! "+a.data.length+"*x != x*"+b.data[0].length));
+			throw(new Exception("Matrix::mult Dimensionen der Zeilen haut nicht hin! "+a.data.length+"*x != x*"+b.data[0].length));
 		if(a.data[0].length!=b.data.length)
-			throw(new Exception("Matrix::Dimensionen der Spalten haut nicht hin! x*"+a.data[0].length+" != "+b.data.length+"*x"));
+			throw(new Exception("Matrix::mult Dimensionen der Spalten haut nicht hin! x*"+a.data[0].length+" != "+b.data.length+"*x"));
 		
 		Matrix erg = new Matrix(a.data.length, b.data[0].length, 0.0);
 		
 		for (int i = 0; i < erg.data.length; i++)
 			for (int j = 0; j < erg.data[0].length; j++)
 				for (int k = 0; k < a.data[0].length; k++)
-					erg.data[i][j]
-							+=
-							a.data[i][k]
-									*
-									b.data[k][j];
+					erg.data[i][j]+=a.data[i][k]*b.data[k][j];
+		return erg;
+	}
+	
+	public static Matrix hadamard(Matrix a, Matrix b) throws Exception
+	{
+		if(a.data.length!=b.data.length)
+			throw(new Exception("Matrix::hadamard Dimensionen der Zeilen haut nicht hin! "+a.data.length+"*x != "+b.data.length+"*x"));
+		if(a.data[0].length!=b.data[0].length)
+			throw(new Exception("Matrix::hadamard Dimensionen der Spalten haut nicht hin! x*"+a.data[0].length+" != x*"+b.data[0].length));
+		Matrix erg=new Matrix(a.data.length, a.data[0].length);
+		for (int i = 0; i < erg.data.length; i++)
+			for (int j = 0; j < erg.data[0].length; j++)
+				erg.data[i][j]=a.data[i][j]*b.data[i][j];
+		return erg;
+	}
+	
+	public static Matrix eye(int zeilen, int spalten)
+	{
+		Matrix erg=new Matrix(zeilen, spalten);
+		for (int i = 0; i < erg.data.length&&i<erg.data[0].length; i++)
+			erg.data[i][i]=1;
+		return erg;
+	}
+		
+	public static Matrix clone(Matrix a)
+	{
+		Matrix erg=new Matrix(a.data.length, a.data[0].length);
+		for (int i = 0; i < a.data.length; i++)
+			erg.data[i]=a.data[i].clone();
+		return erg;
+	}
+	
+	public static Matrix scale(Matrix a, double s)
+	{
+		Matrix erg= new Matrix(a.data.length, a.data[0].length);
+		for (int i = 0; i < a.data.length; i++)
+			for (int j = 0; j < a.data[0].length; j++)
+				erg.data[i][j]=a.data[i][j]*s;
+		return erg;
+	}
+	
+	public static Matrix transpose(Matrix a) 
+	{
+		Matrix erg=new Matrix(a.data[0].length, a.data.length);
+		for (int i = 0; i < a.data.length; i++)
+			for (int j = 0; j < a.data[0].length; j++)
+				erg.data[j][i]=a.data[i][j];
 		return erg;
 	}
 	
@@ -81,11 +124,19 @@ public class Matrix
 	@Override
 	public String toString()
 	{
-		String erg="[";//="Matrix:\n[";
+		int len=0;
+		for (int i = 0; i < data.length; i++)
+			for (int j = 0; j < data[0].length; j++)
+			{	
+				int curr_len=String.format("% f",data[i][j]).length();
+				if(len<curr_len)
+					len=curr_len;
+			}
+		String erg="[";
 		for (int i = 0; i < data.length; i++)
 		{
 			for (int j = 0; j < data[i].length; j++)
-				erg+=data[i][j]+" ";
+				erg+=String.format("% "+len+"f", data[i][j])+" ";
 			erg+="\n ";
 		}
 		erg=erg.substring(0, erg.length()-3);
